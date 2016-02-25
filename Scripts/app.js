d93736e6c265e87a5d8b4c4c991ce9eb3018bf54
antanikmitra <antanikmitra@hotmail.com>
2016-02-25 09:22:21 -0500

    inserting paragraph one

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,29 @@
 // setup your IIFE (Immediately Invoked Function Expression)
+/*Author: Antanik Mitra
+  File: app.js
+  Website name: http://mid-term-comp1073.azurewebsites.net/
+  File description: This is the app.js file where the paragraph is injected and output to the submit button in contact form is displayed in the console.
+*/
 var travelReport = (function () {
 
 "use strict";
 console.log("App Started...")
 
+// replacing paragraph one
+ var replaceFirstParagraph = function () {
+
+        console.log("inside replaceFirstParagraph anonymous Function");
+
+        var IntroParagraph;
+
+        IntroParagraph = document.getElementById("IntroParagraph");
+
+        IntroParagraph.innerHTML = "Now is the time to travel to Greece.The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.Greece has 1,400 islands, though only 230 of them areinhabited.And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.";
 
 
+    };
+// call replaceFirstParagraph
+replaceFirstParagraph();
 
 })();
 
